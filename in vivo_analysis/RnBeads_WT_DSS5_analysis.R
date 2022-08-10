1b66d11b1a12ad55c82ad0560cc6e2c736b3c74d
nehamishra-biogit <74296230+nehamishra-biogit@users.noreply.github.com>
2022-08-10 10:55:24 +0200

    Add files via upload

---
+++ in vivo_analysis/RnBeads_WT_DSS5_analysis.R
@@ -0,0 +1,124 @@
+
+# Template for performing RnBeads analyses - 
+# adjust parameters to get the desired analysis
+
+#path to data.files
+data.dir <- "/work_ifs/sukmb414/DNMT3A/220411_MouseEPIC_Dora/"
+
+idat.dir <- file.path(data.dir, "iDat/")
+
+analysis.dir <- file.path(data.dir, "Analysis/")
+
+report.dir <- file.path(analysis.dir, "reports_WT_DSS5/")
+
+sample.annotation <- file.path(data.dir, "annotation_WT_DSS5.csv")
+data.type<-"idat.dir"
+
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("RnBeads")
+#
+#
+#
+
+library(RnBeads)
+library(RnBeads.mm10)
+
+help("rnb.options")
+rnb.options(analysis.name = "example")
+rnb.options(logging = TRUE)
+#rnb.options(email = "")
+rnb.options(assembly = "mm10")
+rnb.options(identifiers.column = "Sample_ID")
+rnb.options2xml(pretty=TRUE)
+#colors.category = c("darkblue", "darkred")
+rnb.options(min.group.size = 1)
+
+###### modules
+rnb.options(import = TRUE)
+rnb.options(preprocessing = TRUE)
+rnb.options(qc = TRUE)
+rnb.options(inference = FALSE)
+rnb.options(exploratory = TRUE)
+rnb.options(differential = TRUE)
+
+##############some selected options (see more at help("rnb.options"))
+
+#rnb.options(import.default.data.type = "infinium.idat.dir") # NGS: "bs.bed.dir"
+rnb.options(import.table.separator = ",") # for TABdel: "\t"
+rnb.options(import.gender.prediction = FALSE) # only for array datasets
+
+#normalization
+rnb.options(normalization = NULL)
+rnb.options(normalization.method = "wm.dasen")
+rnb.options(normalization.background.method = "none")
+rnb.options(normalization.plot.shifts = TRUE)
+
+rnb.options(qc.boxplots = TRUE)
+rnb.options(qc.barplots = TRUE)
+rnb.options(qc.negative.boxplot = TRUE)
+rnb.options(qc.snp.heatmap = FALSE)
+rnb.options(qc.snp.distances =TRUE)
+rnb.options(qc.snp.boxplot = TRUE)
+rnb.options(qc.snp.barplot = TRUE)
+rnb.options(qc.sample.batch.size = 50)
+rnb.options(qc.coverage.plots = FALSE)
+rnb.options(qc.coverage.threshold.plot = 1:10)
+rnb.options(qc.coverage.histograms = FALSE)
+rnb.options(qc.coverage.violins = FALSE)
+
+rnb.options(filtering.whitelist = NULL)
+rnb.options(filtering.blacklist = NULL)
+rnb.options(filtering.snp = "3")
+rnb.options(filtering.cross.reactive = FALSE)
+rnb.options(filtering.greedycut = TRUE)
+rnb.options(filtering.greedycut.pvalue.threshold = 0.05)
+rnb.options(filtering.greedycut.rc.ties = "row")
+rnb.options(filtering.sex.chromosomes.removal = TRUE)
+rnb.options(filtering.missing.value.quantile = 0.8)
+rnb.options(filtering.coverage.threshold = 5)
+rnb.options(filtering.low.coverage.masking = FALSE)
+rnb.options(filtering.high.coverage.outliers = FALSE)
+rnb.options(filtering.deviation.threshold = 0)
+
+
+#rnb.load.annotation.from.db("ensembleRegBuildBPall")
+#rnb.load.annotation.from.db("ensembleRegBuildBPproximal")
+#rnb.load.annotation.from.db("ensembleRegBuildBPdistal")
+rnb.options(region.types=c("promoters","genes","cpgislands","tiling")) 
+
+
+rnb.options(differential.site.test.method = "limma") #limma"
+rnb.options(differential.comparison.columns = c("Condition"))
+#rnb.options(differential.comparison.columns.all.pairwise = c("CTRL_Vs_MSA_Npos",
+ #                                                            "ExVivoNpos_Vs_InVitroNpos",
+  #                                                           "Npos_Vs_Nneg"))
+
+rnb.options(covariate.adjustment.columns = NULL)
+#rnb.options(columns.pairing = c("week_unified"="code"))
+rnb.options(differential.adjustment.sva = TRUE)
+rnb.options(differential.adjustment.celltype = FALSE)
+rnb.options(differential.enrichment = FALSE)
+rnb.options(differential.report.sites = TRUE)
+
+rnb.options(export.to.bed = TRUE)
+rnb.options(export.to.trackhub = c("bigBed","bigWig"))
+rnb.options(export.to.csv = TRUE) #default = FALSE #creates big tables
+rnb.options(export.to.ewasher = FALSE)
+rnb.options(export.types = "sites")
+
+rnb.options(disk.dump.big.matrices = TRUE)
+rnb.options(enforce.memory.management = TRUE)
+rnb.options(enforce.destroy.disk.dumps = TRUE)
+
+##############################################################################
+
+##### Run Analysis
+rnb.run.analysis(dir.reports=report.dir,
+                 sample.sheet=sample.annotation,
+                 data.dir=idat.dir,
+                 data.type=data.type)
+
+
+##############################################################################
+
+
